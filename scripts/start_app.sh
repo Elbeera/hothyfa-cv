1f30f88af950140b4f2eb0837b29833819a44695
Elbeera <h.s.elbeera@gmail.com>
2026-04-12 21:19:38 +0100

    Fix deploy scripts to release port 3000 before restart

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-cd /home/ubuntu/hothyfa-cv
+export PATH=$PATH:/usr/bin
+
+cd /home/ubuntu/hothyfa-cv || exit 1
 
 pm2 start npm --name "hothyfa-cv" -- start
 pm2 save
--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
+export PATH=$PATH:/usr/bin
+
+cd /home/ubuntu/hothyfa-cv || exit 1
+
 pm2 stop hothyfa-cv || true
 pm2 delete hothyfa-cv || true
+
+# Kill anything still listening on port 3000
+fuser -k 3000/tcp || true
+sleep 2
