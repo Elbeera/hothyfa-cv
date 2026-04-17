d8664ce482c7ad61d792e72bf2af7aade070c6dd
Elbeera <h.s.elbeera@gmail.com>
2026-04-17 22:40:34 +0100

    update build_app.sh

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -6,4 +6,7 @@ cd /home/ubuntu/hothyfa-cv
 # Remove stray lockfile if it ever reappears
 rm -f /home/ubuntu/package-lock.json
 
+# Public API URL for the AI CV Assistant
+export NEXT_PUBLIC_AI_CV_API_URL=http://98.93.23.43:8000
+
 npm run build
